a22db36568db929806e39ae6fbc09cb0c780de22
Julian Ganguli <julianganguli@gmail.com>
2017-07-29 17:28:48 -0700

    adding base and project files

---
+++ models/base/segment_pages.sql
@@ -0,0 +1,21 @@
+SELECT
+  id,
+  received_at,
+  timestamp,
+  anonymous_id,
+  user_id,
+  name,
+  category,
+  path,
+  url,
+  title,
+  context_page_referrer as referrer,
+  search,
+  context_campaign_name,
+  context_campaign_content,
+  context_campaign_medium,
+  context_campaign_source,
+  context_campaign_term,
+  context_user_agent,
+from
+  {{ var('base.pages') }}
