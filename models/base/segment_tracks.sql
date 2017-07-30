8eb4bf2ea21076f46ce17cefae5542650b1250e3
Julian Ganguli <julianganguli@gmail.com>
2017-07-29 17:42:06 -0700

    adding and fixing base models

---
+++ models/base/segment_tracks.sql
@@ -0,0 +1,21 @@
+select
+  id,
+  received_at,
+  a.timestamp,
+  anonymous_id,
+  user_id,
+  event,
+  event_text,
+  context_page_path as path,
+  context_page_url as url,
+  context_page_title as title,
+  context_page_referrer as referrer,
+  context_page_search as search,
+  context_campaign_name as campaign_name,
+  context_campaign_content as campaign_content,
+  context_campaign_medium as campaign_medium,
+  context_campaign_source as campaign_source,
+  context_campaign_term as campaign_term,
+  context_user_agent
+from 
+  {{ var('base.tracks') }} a
